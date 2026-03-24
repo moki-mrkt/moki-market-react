bf4292043ca57b8d8cf45f2c518b8d271dd961c8
kirillpivovarov <kirilp@bandbsolution.com.ua>
2026-03-24 03:58:45 +0200

    changed user photo logic and width feedback in mobile

--- src/components/UserRoom/UserRoom.jsx
+++ src/components/UserRoom/UserRoom.jsx
@@ -117,7 +117,7 @@ const UserRoom = () => {
                     >
                         {user.imageUrl ? (
                             <>
-                                <img src={`${image_api}${user.imageUrl}`} alt="User Avatar" className="avatar-image" />
+                                <img src={`https://images.moki.com.ua/${user.imageUrl}`} alt="User Avatar" className="avatar-image" />
                                 <div className="avatar-overlay delete-overlay" onClick={handleDeleteAvatar}>
                                     <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" ><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                                 </div>
