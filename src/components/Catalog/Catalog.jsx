7809425ee02d0118d76867a5688c8c31dbd48d65
kirillpivovarov <kirilp@bandbsolution.com.ua>
2026-03-30 14:26:33 +0300

    changed photo categories format to webp

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -6,17 +6,17 @@ import {Helmet} from "react-helmet-async";
 
 const Catalog = () => {
     const categories = [
-        { id: 1, title: 'Сухофрукти', img: '/img/categories/dry_fruits.png', link: '/catalog/dried-fruits' },
-        { id: 2, title: 'Солодощі', img: '/img/categories/sweets.png', link: '/catalog/sweets' },
-        { id: 3, title: 'Цукерки', img: '/img/categories/candies.png', link: '/catalog/candies' },
-        { id: 4, title: 'Горіхи', img: '/img/categories/nuts.png', link: '/catalog/nuts' },
-        { id: 5, title: 'Кава', img: '/img/categories/coffee.png', link: '/catalog/coffee' },
-        { id: 6, title: 'Чай', img: '/img/categories/tea.png', link: '/catalog/tea' },
-        { id: 7, title: 'Суперфуд', img: '/img/categories/superfood.png', link: '/catalog/superfoods' },
-        { id: 8, title: 'Олія та масла', img: '/img/categories/oils.png', link: '/catalog/oils' },
-        { id: 9, title: 'Консервація', img: '/img/categories/preserves.png', link: '/catalog/preserves' },
-        { id: 10, title: 'Снеки та чіпси', img: '/img/categories/snecks.png', link: '/catalog/snacks' },
-        { id: 11, title: 'Спеції', img: '/img/categories/spices.png', link: '/catalog/spices' },
+        { id: 1, title: 'Сухофрукти', img: '/img/categories/dry_fruits.webp', link: '/catalog/dried-fruits' },
+        { id: 2, title: 'Солодощі', img: '/img/categories/sweets.webp', link: '/catalog/sweets' },
+        { id: 3, title: 'Цукерки', img: '/img/categories/candies.webp', link: '/catalog/candies' },
+        { id: 4, title: 'Горіхи', img: '/img/categories/nuts.webp', link: '/catalog/nuts' },
+        { id: 5, title: 'Кава', img: '/img/categories/coffee.webp', link: '/catalog/coffee' },
+        { id: 6, title: 'Чай', img: '/img/categories/tea.webp', link: '/catalog/tea' },
+        { id: 7, title: 'Суперфуд', img: '/img/categories/superfood.webp', link: '/catalog/superfoods' },
+        { id: 8, title: 'Олія та масла', img: '/img/categories/oils.webp', link: '/catalog/oils' },
+        { id: 9, title: 'Консервація', img: '/img/categories/preserves.webp', link: '/catalog/preserves' },
+        { id: 10, title: 'Снеки та чіпси', img: '/img/categories/snecks.webp', link: '/catalog/snacks' },
+        { id: 11, title: 'Спеції', img: '/img/categories/spices.webp', link: '/catalog/spices' },
     ];
 
     return (
